d594342077a4c5d36edce89a2984bffdf6020992
Hareesh Makam <makamhareesh@gmail.com>
2017-11-13 21:24:56 -0800

    changed paths to match valohai locations

--- train.py
+++ train.py
@@ -13,6 +13,8 @@ import pandas as pd
 import csv
 import numpy as np
 from ast import literal_eval
+from shutil import copy2
+import tarfile
 
 
 # you need to change this to your data directory
@@ -20,7 +22,7 @@ train_dir ='F:/Udacity ML/git/udacity-capstone'
 #Function to load the list of image file paths and its labels.
 def get_files():
     '''
-    
+
     Returns:
         list of images and labels
 
@@ -29,18 +31,25 @@ def get_files():
     print ("Loading files...")
     image_list=[]
     label_list=[]
-    trainpath="/Users/hmakam200/Desktop/ramya_data/FinalData_256/"
+    trainpath="/valohai/inputs/training-set-images/FinalData_256.tgz"
+
+    train_dir = os.getcwd()
+    copy2(trainpath, train_dir)
+    tar = tarfile.open(train_dir+"/FinalData_256.tgz")
+    tar.extractall(path=train_dir)
+    tar.close()
+
     count=0
     drimage=0
-    with open('ramyaList.csv') as f:
+    with open('/valohai/inputs/training-set-labels/ramyaList.l3.csv') as f:
         reader = csv.reader(f,delimiter=',')
-        for row in reader:                        
-            image_list.append(trainpath+row[0]+".jpeg")
-            temp=literal_eval(row[1])            
+        for row in reader:
+            image_list.append(train_dir+row[0]+".jpeg")
+            temp=literal_eval(row[1])
             label_list.append(temp)
             count=count+1
             if temp==1:
-                drimage=drimage+1  
+                drimage=drimage+1
             if count==6000:
                 break
     #print (type(image_list[0]))
@@ -62,41 +71,41 @@ def get_batch(image, label, image_W, image_H, batch_size, capacity):
     Returns:
         image_batch: 4D tensor [batch_size, width, height, 3], dtype=tf.float32
         label_batch: 1D tensor [batch_size], dtype=tf.int32
-    '''       
+    '''
     image = tf.cast(image, tf.string)#cast image list values to string tensors
     label = tf.to_int32(label)
-    
+
     input_queue = tf.train.slice_input_producer([image, label]) #Produces a slice of each Tensor in [image,label] list.
     #num_epochs: An integer (optional). If specified, slice_input_producer produces each slice num_epochs times
-    #before generating an OutOfRange error. 
+    #before generating an OutOfRange error.
     #If not specified, slice_input_producer can cycle through the slices an unlimited number of times.
-    
+
     label = input_queue[1]
-    
+
     image_contents = tf.read_file(input_queue[0])
     image = tf.image.decode_jpeg(image_contents, channels=3)
 
     # data argumentation should go to here ########################################
-    
+
     image = tf.image.resize_image_with_crop_or_pad(image, image_W, image_H)
-    
-    
+
+
     image = tf.image.per_image_standardization(image)
     image=tf.image.random_flip_left_right(image)
     image=tf.image.random_brightness(image, max_delta=0.3)
-    image= tf.image.random_contrast(image, 0.8, 1.2)    
+    image= tf.image.random_contrast(image, 0.8, 1.2)
     image=tf.image.random_flip_up_down(image)
     image=tf.image.random_hue(image,max_delta=0.3)
-    
+
     image_batch, label_batch = tf.train.batch([image, label],
                                                 batch_size= batch_size,
-                                                num_threads= 2, 
+                                                num_threads= 2,
                                                 capacity = capacity)
-    
-    
+
+
     label_batch = tf.reshape(label_batch, [batch_size])
     image_batch = tf.cast(image_batch, tf.float32)
-    
+
     return image_batch, label_batch
 x,y=get_files()
 
@@ -116,21 +125,21 @@ def inference(images, batch_size, n_classes,keep_prob):
         output tensor with the computed logits, float, [batch_size, n_classes]
     '''
     #conv1, shape = [kernel size, kernel size, channels, kernel numbers]
-    
+
     with tf.variable_scope('conv1') as scope:
-        weights = tf.get_variable('weights', 
+        weights = tf.get_variable('weights',
                                   shape = [3,3,3, 16],
-                                  dtype = tf.float32, 
+                                  dtype = tf.float32,
                                   initializer=tf.truncated_normal_initializer(stddev=0.1,dtype=tf.float32))
-        biases = tf.get_variable('biases', 
+        biases = tf.get_variable('biases',
                                  shape=[16],
                                  dtype=tf.float32,
                                  initializer=tf.constant_initializer(0.1))
         conv = tf.nn.conv2d(images, weights, strides=[1,1,1,1], padding='SAME')
         pre_activation = tf.nn.bias_add(conv, biases)
         conv1 = tf.nn.relu(pre_activation, name= scope.name)
-    
-        
+
+
     #conv2
     with tf.variable_scope('conv2') as scope:
         weights = tf.get_variable('weights',
@@ -138,14 +147,14 @@ def inference(images, batch_size, n_classes,keep_prob):
                                   dtype=tf.float32,
                                   initializer=tf.truncated_normal_initializer(stddev=0.1,dtype=tf.float32))
         biases = tf.get_variable('biases',
-                                 shape=[32], 
+                                 shape=[32],
                                  dtype=tf.float32,
                                  initializer=tf.constant_initializer(0.1))
         conv = tf.nn.conv2d(conv1, weights, strides=[1,1,1,1],padding='SAME')
         pre_activation = tf.nn.bias_add(conv, biases)
         conv2 = tf.nn.relu(pre_activation, name='conv2')
-    
-    
+
+
     #pool2 and norm2
     with tf.variable_scope('pooling1_lrn') as scope:
         pool1 = tf.nn.max_pool(conv2, ksize=[1,3,3,1],strides=[1,2,2,1],
@@ -159,15 +168,15 @@ def inference(images, batch_size, n_classes,keep_prob):
                                   dtype=tf.float32,
                                   initializer=tf.truncated_normal_initializer(stddev=0.1,dtype=tf.float32))
         biases = tf.get_variable('biases',
-                                 shape=[64], 
+                                 shape=[64],
                                  dtype=tf.float32,
                                  initializer=tf.constant_initializer(0.1))
         conv = tf.nn.conv2d(norm1, weights, strides=[1,1,1,1],padding='SAME')
         pre_activation = tf.nn.bias_add(conv, biases)
         conv3 = tf.nn.relu(pre_activation, name='conv3')
-    
-    
-   
+
+
+
     #conv4
     with tf.variable_scope('conv4') as scope:
         weights = tf.get_variable('weights',
@@ -175,14 +184,14 @@ def inference(images, batch_size, n_classes,keep_prob):
                                   dtype=tf.float32,
                                   initializer=tf.truncated_normal_initializer(stddev=0.1,dtype=tf.float32))
         biases = tf.get_variable('biases',
-                                 shape=[128], 
+                                 shape=[128],
                                  dtype=tf.float32,
                                  initializer=tf.constant_initializer(0.1))
         conv = tf.nn.conv2d(conv3, weights, strides=[1,1,1,1],padding='SAME')
         pre_activation = tf.nn.bias_add(conv, biases)
         conv4 = tf.nn.relu(pre_activation, name='conv4')
-    
-    
+
+
     #pool4 and norm4
     with tf.variable_scope('pooling2_lrn') as scope:
         pool2 = tf.nn.max_pool(conv4, ksize=[1,3,3,1],strides=[1,2,2,1],
@@ -196,35 +205,35 @@ def inference(images, batch_size, n_classes,keep_prob):
                                   dtype=tf.float32,
                                   initializer=tf.truncated_normal_initializer(stddev=0.1,dtype=tf.float32))
         biases = tf.get_variable('biases',
-                                 shape=[128], 
+                                 shape=[128],
                                  dtype=tf.float32,
                                  initializer=tf.constant_initializer(0.1))
         conv = tf.nn.conv2d(norm2, weights, strides=[1,1,1,1],padding='SAME')
         pre_activation = tf.nn.bias_add(conv, biases)
         conv5 = tf.nn.relu(pre_activation, name='conv5')
-    
-    
+
+
     with tf.variable_scope('conv6') as scope:
         weights = tf.get_variable('weights',
                                   shape=[3,3,128,256],
                                   dtype=tf.float32,
                                   initializer=tf.truncated_normal_initializer(stddev=0.1,dtype=tf.float32))
         biases = tf.get_variable('biases',
-                                 shape=[256], 
+                                 shape=[256],
                                  dtype=tf.float32,
                                  initializer=tf.constant_initializer(0.1))
         conv = tf.nn.conv2d(conv5, weights, strides=[1,1,1,1],padding='SAME')
         pre_activation = tf.nn.bias_add(conv, biases)
         conv6 = tf.nn.relu(pre_activation, name='conv6')
-    
-    
+
+
     #pool6 and norm6
     with tf.variable_scope('pooling3_lrn') as scope:
         norm3 = tf.nn.lrn(conv6, depth_radius=4, bias=1.0, alpha=0.001/9.0,
                           beta=0.75,name='norm3')
         pool3 = tf.nn.max_pool(norm3, ksize=[1,3,3,1], strides=[1,1,1,1],
                                padding='SAME',name='pooling3')
-        
+
     #local7
     with tf.variable_scope('local7') as scope:
         reshape = tf.reshape(pool3, shape=[batch_size, -1])
@@ -235,25 +244,25 @@ def inference(images, batch_size, n_classes,keep_prob):
                                   initializer=tf.truncated_normal_initializer(stddev=0.005,dtype=tf.float32))
         biases = tf.get_variable('biases',
                                  shape=[1024],
-                                 dtype=tf.float32, 
+                                 dtype=tf.float32,
                                  initializer=tf.constant_initializer(0.1))
-        local7 = tf.nn.relu(tf.matmul(reshape, weights) + biases, name=scope.name) 
-        local7=tf.nn.dropout(local7, keep_prob) 
-    
-   
-        
+        local7 = tf.nn.relu(tf.matmul(reshape, weights) + biases, name=scope.name)
+        local7=tf.nn.dropout(local7, keep_prob)
+
+
+
     # softmax
     with tf.variable_scope('softmax_linear') as scope:
         weights = tf.get_variable('softmax_linear',
                                   shape=[1024, n_classes],
                                   dtype=tf.float32,
                                   initializer=tf.truncated_normal_initializer(stddev=0.005,dtype=tf.float32))
-        biases = tf.get_variable('biases', 
+        biases = tf.get_variable('biases',
                                  shape=[n_classes],
-                                 dtype=tf.float32, 
+                                 dtype=tf.float32,
                                  initializer=tf.constant_initializer(0.1))
         softmax_linear = tf.add(tf.matmul(local7, weights), biases, name='softmax_linear')
-    
+
     return softmax_linear
 
 
@@ -262,7 +271,7 @@ def losses(logits, labels):
     Args:
         logits: logits tensor, float, [batch_size, n_classes]
         labels: label tensor, tf.int32, [batch_size]
-        
+
     Returns:
         loss tensor of float type
     '''
@@ -274,12 +283,12 @@ def losses(logits, labels):
 
 
 def trainning(loss, learning_rate):
-    '''Training ops, the Op returned by this function is what must be passed to 
+    '''Training ops, the Op returned by this function is what must be passed to
         'sess.run()' call to cause the model to train.
-        
+
     Args:
         loss: loss tensor, from losses()
-        
+
     Returns:
         train_op: The op for trainning
     '''
@@ -300,17 +309,17 @@ def evaluation(logits, labels):
         A scalar int32 tensor with the number of examples (out of batch_size)
         that were predicted correctly.
     """
-    
-    
+
+
     with tf.variable_scope('accuracy') as scope:
-        
+
         correct = tf.nn.in_top_k(logits, labels, 1)
         correct = tf.cast(correct, tf.float16)
-        accuracy = tf.reduce_mean(correct) 
+        accuracy = tf.reduce_mean(correct)
         #roc_score = tf.contrib.metrics.streaming_auc(labels, correct)
         tf.summary.scalar(scope.name+'/accuracy', accuracy)
         #tf.summary.scalar(scope.name+'/roc_score',roc_score)
-        
+
     #return accuracy,roc_score
     return accuracy
 
@@ -332,31 +341,31 @@ IMG_W = 256  # resize the image, if the input image is too large, training will 
 IMG_H = 256
 BATCH_SIZE = 32
 CAPACITY = 20
-MAX_STEP = 150 
-learning_rate = 0.01 
+MAX_STEP = 150
+learning_rate = 0.01
 
 
 #%%
 def run_training():
-    
+
     # you need to change the directories to yours.
-    
+
     logs_train_dir = '/Users/hmakam200/personal/tensorflow/ramya-capstone/board/'
-    
+
     train, train_label = get_files()
     keep_prob=tf.placeholder(tf.float32,name='keep_prob')
-    
-    train_batch, train_label_batch = get_batch(train,train_label,IMG_W,IMG_H,BATCH_SIZE,CAPACITY)      
+
+    train_batch, train_label_batch = get_batch(train,train_label,IMG_W,IMG_H,BATCH_SIZE,CAPACITY)
     train_logits = inference(train_batch, BATCH_SIZE, N_CLASSES,keep_prob=0.7)
-    train_loss = losses(train_logits, train_label_batch)        
+    train_loss = losses(train_logits, train_label_batch)
     train_op = trainning(train_loss, learning_rate)
     train__acc =evaluation(train_logits, train_label_batch)
-       
+
     summary_op = tf.summary.merge_all()
     sess = tf.Session()
     train_writer = tf.summary.FileWriter(logs_train_dir, sess.graph)
     saver = tf.train.Saver()
-    
+
     sess.run(tf.global_variables_initializer())
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(sess=sess, coord=coord)
@@ -366,33 +375,33 @@ def run_training():
             if coord.should_stop():
                     break
             _, tra_loss, tra_acc = sess.run([train_op, train_loss, train__acc])
-               
+
             if step % 25 == 0:
                 print('Step %d, train loss = %.2f, train accuracy = %.2f%%' %(step, tra_loss, tra_acc*100.0))
                 f.write("%i %5.2f\n" % (step, tra_acc*100.0))
                 summary_str = sess.run(summary_op)
                 train_writer.add_summary(summary_str, step)
-            
+
             if step % 50 == 0 or (step + 1) == MAX_STEP:
                 checkpoint_path = os.path.join(logs_train_dir, 'model.ckpt')
                 saver.save(sess, checkpoint_path, global_step=step)
-                
+
     except tf.errors.OutOfRangeError:
         print('Done training -- epoch limit reached')
     finally:
         coord.request_stop()
-        
+
     coord.join(threads)
     sess.close()
     f.close()
-    
+
 
 
 
 # In[ ]:
 
 
-tf.reset_default_graph() 
+tf.reset_default_graph()
 print ("Training started.")
 
 run_training()
@@ -425,20 +434,20 @@ def get_files_test():
 
     '''
     print(tf.__version__)
-    
+
     image_list=[]
     label_list=[]
     testpath="/Users/hmakam200/Desktop/ramya_data/FinalData_256/"
     with open('test10.csv') as f:
         reader = csv.reader(f,delimiter=',')
         for row in reader:
-                        
+
             image_list.append(testpath+row[0]+".jpeg")
-            temp=literal_eval(row[1])            
+            temp=literal_eval(row[1])
             label_list.append(temp)
     #print (type(image_list[0]))
     #print (type(label_list[0]))
-    
+
     return image_list, label_list
 
 def get_batch_test(image, label, image_W, image_H, batch_size, capacity):
@@ -454,40 +463,40 @@ def get_batch_test(image, label, image_W, image_H, batch_size, capacity):
         image_batch: 4D tensor [batch_size, width, height, 3], dtype=tf.float32
         label_batch: 1D tensor [batch_size], dtype=tf.int32
     '''
-    
+
     image = tf.cast(image, tf.string)#cast image list values to string tensors
-    label = tf.to_int32(label)  
+    label = tf.to_int32(label)
 
 
     # make an input queue
-    input_queue = tf.train.slice_input_producer([image, label])    
+    input_queue = tf.train.slice_input_producer([image, label])
     label = input_queue[1]
-    
+
     image_contents = tf.read_file(input_queue[0])
-    
+
     image = tf.image.decode_jpeg(image_contents, channels=3)
-    
+
     ######################################
     # data argumentation should go to here
     ######################################
-    
+
     image = tf.image.per_image_standardization(image)
     image = tf.image.resize_image_with_crop_or_pad(image, image_W, image_H)
-    
-    
-    
-    
-          
+
+
+
+
+
     image_batch, label_batch = tf.train.batch([image, label],
                                                 batch_size= batch_size,
-                                                num_threads= 2, 
+                                                num_threads= 2,
                                                 capacity = capacity)
-    
-    
-    
+
+
+
     label_batch = tf.reshape(label_batch, [batch_size])
     image_batch = tf.cast(image_batch, tf.float32)
-    
+
     return image_batch, label_batch
 
 
@@ -516,32 +525,32 @@ def get_one_image(train):
     return image
 
 def evaluate_test():
-    
-    IMG_W = 256 
+
+    IMG_W = 256
     IMG_H = 256
     BATCH_SIZE = 2
     CAPACITY = 2
     N_CLASSES=2
-    tf.reset_default_graph() 
+    tf.reset_default_graph()
     test, test_label = get_files_test()#load the test image paths and labels
-  
-    
-    test_batch, test_label_batch = get_batch_test(test,test_label,IMG_W,IMG_H,BATCH_SIZE,CAPACITY)#get images and lables batches  
-       
+
+
+    test_batch, test_label_batch = get_batch_test(test,test_label,IMG_W,IMG_H,BATCH_SIZE,CAPACITY)#get images and lables batches
+
     coord = tf.train.Coordinator()
-    
-    
-    #with tf.Graph().as_default():            
-             
-    logit = inference(test_batch, BATCH_SIZE, N_CLASSES)        
+
+
+    #with tf.Graph().as_default():
+
+    logit = inference(test_batch, BATCH_SIZE, N_CLASSES)
     logit = tf.nn.softmax(logit)
     x = tf.placeholder(tf.float32, shape=[BATCH_SIZE,256, 256, 3])
-        
+
     # you need to change the directories to yours.
-    logs_train_dir = '/Users/hmakam200/personal/tensorflow/ramya-capstone/board/' 
-                       
+    logs_train_dir = '/Users/hmakam200/personal/tensorflow/ramya-capstone/board/'
+
     saver = tf.train.Saver()
-        
+
     with tf.Session() as sess:
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(sess=sess, coord=coord)
@@ -553,7 +562,7 @@ def evaluate_test():
             print('Loading success, global_step is %s' % global_step)
         else:
             print('No checkpoint file found')
-            
+
         prediction = sess.run(logit)
         test_acc= sess.run(evaluation(prediction, test_label_batch))
         print (test_acc)
@@ -566,11 +575,11 @@ def evaluate_test():
         sess.run(tf.initialize_local_variables()) # try commenting this line and you'll get the error
         roc_score = sess.run(auc)
         print(roc_score)
-            
+
     coord.request_stop()
-           
+
     coord.join(threads)
-    sess.close()          
+    sess.close()
 
 
 evaluate_test()
@@ -578,7 +587,3 @@ print ("Done")
 
 
 # In[ ]:
-
-
-
-
